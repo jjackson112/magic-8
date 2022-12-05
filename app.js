de8080169b7dc4923ecf6bc0dfd47426f2ee4d54
J. Jackson <jjackson11211@gmail.com>
2022-12-05 11:39:10 -0500

    JS
    array of answers

---
+++ app.js
@@ -0,0 +1,20 @@
+const points = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",	
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Very doubtful",
+    "Outlook not so good",
+    "My sources say no",
+    "My reply is no",
+    "Not today"
+];
